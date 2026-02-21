d446b517b88e9e922c99e4d6cb89338afb1f3d2f
ajsand <sandhara@oregonstate.edu>
2026-02-21 00:43:25 -0800

    Enhance ESLint configuration and improve DeckScreen styles
    Updated the ESLint configuration to include additional ignores for build directories and integrated Prettier for code formatting. Improved the DeckScreen by refining the link presentation and adding padding to enhance the user interface.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,16 @@
 import expoConfig from 'eslint-config-expo/flat.js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default [
   ...expoConfig,
   {
-    ignores: ['node_modules/', '.expo/', 'babel.config.js'],
+    ignores: [
+      'node_modules/',
+      '.expo/',
+      'dist/',
+      'web-build/',
+      'babel.config.js',
+    ],
   },
+  eslintConfigPrettier,
 ];
